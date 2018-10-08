25ebbd824108cc99548186006a247505c5705f73
asanama <43213948+asanama@users.noreply.github.com>
2018-10-08 22:38:14 +0200

    Add files via upload

---
+++ kata2-v4/Kata2.java
@@ -0,0 +1,12 @@
+package kata2;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        Integer[] data = {1,2,4,6,2,4,32,5,1,3,4,5,1,2,5,6,1,2,8};
+        Histogram his = new Histogram(data);
+        System.out.println(his);
+       
+    }
+    
+}
---
+++ kata2-v4/Mapa.java
@@ -0,0 +1,39 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapa {
+    private Map<Integer, Integer> count;
+    
+    
+    public Mapa(){
+         count = new HashMap<>();
+    }
+    
+    public void putValue(int key){
+        count.put(key, (isKey(key)) ? count.get(key)+1: 1);
+    }
+    
+    public boolean isKey(int key){
+        return count.containsKey(key);
+    }
+    
+    @Override
+    public String toString(){
+        String s = "Mapa:\n";
+        int i = 0;
+        for(Map.Entry<Integer,Integer> entry : count.entrySet()){
+            s += entry.getKey() + " --> " + entry.getValue() + "\n";
+            i+= entry.getValue();
+        }
+        s += "Valores: " + i;
+        return s;
+    }
+
+    public Map<Integer, Integer> getMap() {
+        return count;
+    }
+    
+    
+}
