971dc35d878e6adff5f0a7366cb4c610fea84903
asanama <43213948+asanama@users.noreply.github.com>
2018-10-08 22:38:48 +0200

    Add files via upload

---
+++ kata2-v5/Kata2.java
@@ -0,0 +1,14 @@
+package kata2;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        String[] data = {"Aarón", "Alejandro", "Gabriel", "Alejandro",
+        "Aaron"};
+        
+        Histogram his = new Histogram(data);
+        System.out.println(his);
+       
+    }
+    
+}
